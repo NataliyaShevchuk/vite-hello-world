9c3f8843b23f66c55deeccf7fdc835f629742236
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-12-01 17:52:53 +0100

    aggiunto il file AppTitle al file principale

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
+
     export default {
-      data() {
-        return{
-          title: "La mia prima app con Vite!"
-        }
-      }
-    }
+    components: { AppTitle },
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        };
+    },
+}
 </script>
 
 <template>
-  <h1>
-    {{title}}
-  </h1>
-  <p class="lead">Scritto in un paragrafo</p>
+  <AppTitle></AppTitle>
 </template>
 
 
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,6 +1,19 @@
 <script>
+export default{
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    },
+}
 </script>
 
+
 <template>
-    
+    <h1> {{title}} </h1>
+    <p class="lead">Scritto in un paragrafo</p>
 </template>
+
+<style>
+
+</style>
